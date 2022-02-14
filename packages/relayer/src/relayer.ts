67a6ebb7c8a1a58d0e9c1a83423f9274e1094602
Enrique Ortiz <hi@enriqueortiz.dev>
2022-02-14 19:20:55 -0400

    feat(relayer): start relayer class impl

--- packages/relayer/src/relayer.ts
+++ packages/relayer/src/relayer.ts
@@ -1,5 +1,54 @@
-export class Relayer {
-  static gm() {
-    console.log('gm')
+import { AbstractProvider } from '@pokt-foundation/pocketjs-provider'
+import { AbstractSigner, KeyManager } from '@pokt-foundation/pocketjs-signer'
+import {
+  HTTPMethod,
+  Node,
+  PocketAAT,
+  RelayHeaders,
+  RelayResponse,
+  Session,
+  SessionHeader,
+} from '@pokt-foundation/pocketjs-types'
+import { AbstractRelayer } from './abstract-relayer'
+
+export class Relayer implements AbstractRelayer {
+  readonly keyManager: KeyManager | AbstractSigner
+  readonly provider: AbstractProvider
+  readonly dispatchers: string[]
+
+  constructor({ keyManager, provider, dispatchers }) {
+    this.keyManager = keyManager
+    this.provider = provider
+    this.dispatchers = dispatchers
   }
+
+  getNewSession({
+    pocketAAT,
+    chain,
+    options,
+  }: {
+    pocketAAT: PocketAAT
+    chain: string
+    options?: { retryAttempts: number; rejectSelfSignedCertificates: boolean }
+  }): Session {}
+
+  relay({
+    data,
+    blockchain,
+    pocketAAT,
+    headers,
+    method,
+    session,
+    node,
+    path,
+  }: {
+    data: string
+    blockchain: string
+    pocketAAT: PocketAAT
+    headers: RelayHeaders
+    method: HTTPMethod
+    session: Session
+    node: Node
+    path: string
+  }): Promise<RelayResponse> {}
 }

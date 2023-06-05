2b78ec71f8b0e795808a521fcd8fc59b19e432a0
poktblade <baaspoolsllc@gmail.com>
2023-06-04 19:34:57 -0500

    fix long import, pnpm i install

--- packages/transaction-builder/src/models/proto/generated/google/protobuf/any.ts
+++ packages/transaction-builder/src/models/proto/generated/google/protobuf/any.ts
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import Long from 'long'
-import * as _m0 from 'protobufjs/minimal'
+
+import _m0 from 'protobufjs/minimal'
 
 export const protobufPackage = 'google.protobuf'
 

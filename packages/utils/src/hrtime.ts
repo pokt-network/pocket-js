bec25cfcd1bb3b615836ff478efb771a4c6d27fd
Kevin Utoft <kevin.utoft@gmail.com>
2023-10-17 22:49:41 -0500

    Fix Isomorphic Provider (#94)
    * update hrtime cause it was causing problems loading on the browser
    
    * add back polyfill
    
    * update version

--- packages/utils/src/hrtime.ts
+++ packages/utils/src/hrtime.ts
@@ -1,7 +1,7 @@
 const _perfomancePolyfill = () => {
   // based on https://gist.github.com/paulirish/5438650 copyright Paul Irish 2015.
   if ('performance' in window === false) {
-    (window.performance as unknown) = {}
+    ;(window.performance as unknown) = {}
   }
 
   Date.now =
@@ -24,6 +24,16 @@ const _perfomancePolyfill = () => {
 
 const _hrtime = (previousTimestamp?: [number, number]): [number, number] => {
   _perfomancePolyfill()
+
+  if (
+    (typeof process === 'undefined' || typeof process.hrtime === 'undefined') &&
+    typeof window.process === 'undefined'
+  ) {
+    console.log('window.process undefined')
+    window.process = {} as NodeJS.Process
+    window.process.hrtime = _hrtime
+  }
+
   const baseNow = Math.floor((Date.now() - performance.now()) * 1e-3)
   const clocktime = performance.now() * 1e-3
   let seconds = Math.floor(clocktime) + baseNow
@@ -39,18 +49,11 @@ const _hrtime = (previousTimestamp?: [number, number]): [number, number] => {
   }
   return [seconds, nanoseconds]
 }
+
 const NS_PER_SEC = 1e9
 _hrtime.bigint = (time?: [number, number]): bigint => {
   const diff = _hrtime(time)
   return (diff[0] * NS_PER_SEC + diff[1]) as unknown as bigint
 }
-if (
-  (typeof process === 'undefined' || typeof process.hrtime === 'undefined') &&
-  typeof window.process === 'undefined'
-) {
-  window.process = {} as NodeJS.Process
-}
-export const hrtime =
-  typeof process.hrtime === 'undefined'
-    ? (window.process.hrtime = _hrtime)
-    : process.hrtime
+
+export const hrtime = _hrtime

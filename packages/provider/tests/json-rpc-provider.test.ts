pokt-network/pocket-js

commit 1766e700146ee1d8da5df86b49c00689545849d3

JsonRpcProvider: Basic access authentication support (#97)
With this patch, `JsonRpcProvider` accepts a url with basic authentication
credentials like this:

```
const PoktEndpoint = 'https://scott:tiger@pocket-node.example.com';
const provider = new JsonRpcProvider({
  rpcUrl: PoktEndpoint,
  dispatchers: [PoktEndpoint],
});
```

packages/provider/src/json-rpc-provider.ts
@@ -1,5 +1,6 @@
 import AbortController from 'abort-controller'
 import debug from 'debug'
+import { Buffer } from 'buffer'
 import { fetch, Response } from 'undici'
 import {
   Account,
@@ -93,14 +94,25 @@ export class JsonRpcProvider implements AbstractProvider {
         ]
       : this.rpcUrl
 
-    const routedRpcUrl = `${finalRpcUrl}${route}`
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const url = new URL(route, finalRpcUrl);
+    if (url.username || url.password) {
+      const credential =
+        Buffer.from(`${url.username}:${url.password}`).toString('base64');
+      headers['Authorization'] = `Basic ${credential}`;
+      url.username = '';
+      url.password = '';
+    }
+
+    const routedRpcUrl = url.toString();
 
     const rpcResponse = await fetch(routedRpcUrl, {
       method: 'POST',
       signal: controller.signal as AbortSignal,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: headers,
       body: JSON.stringify(body),
     }).catch((error) => {
       debug(`${routedRpcUrl} attempt ${retriesPerformed + 1} failure`)

commit b73415dcfd4a81939873cf836be76b8c3b16f2f9

URL with a path and credentials is not handled correctly (#103)
This is a fix for a regression introduced by 1766e700146ee1d8da5df86b49c00689545849d3.

If a URL with a path is passed to `JsonRpcProvider`, the path is dropped when the provider makes a query because `const url = new URL(route, finalRpcUrl);` does drop the path in `finalRpcUrl`.

The proposed fix to create a URL first, and appends a path via string concat.  This patch also adds a testcase for that logic.

packages/provider/src/json-rpc-provider.ts
@@ -36,6 +36,28 @@ import {
 const DEFAULT_TIMEOUT = 10000
 
 /**
+ * extractBasicAuth extracts basic authentication credentials from the given
+ * url into a separate base64-encoded string so that we can pass it to Fetch API.
+ **/
+export function extractBasicAuth(urlStr: string) {
+  const url = new URL(urlStr);
+  if (!url.username && !url.password) {
+    return {
+      urlStr,
+    };
+  }
+
+  const credential =
+    Buffer.from(`${url.username}:${url.password}`).toString('base64');
+  url.username = '';
+  url.password = '';
+  return {
+    urlStr: url.toString(),
+    basicAuth: `Basic ${credential}`,
+  };
+}
+
+/**
  * A JSONRPCProvider lets you query data from the chain and send relays to the network.
  * NodeJS only, not Isomorphic or Browser compatible.
  *  **/
@@ -98,16 +120,12 @@ export class JsonRpcProvider implements AbstractProvider {
       'Content-Type': 'application/json',
     };
 
-    const url = new URL(route, finalRpcUrl);
-    if (url.username || url.password) {
-      const credential =
-        Buffer.from(`${url.username}:${url.password}`).toString('base64');
-      headers['Authorization'] = `Basic ${credential}`;
-      url.username = '';
-      url.password = '';
+    const {urlStr, basicAuth} = extractBasicAuth(finalRpcUrl);
+    if (basicAuth) {
+      headers['Authorization'] = basicAuth;
     }
 
-    const routedRpcUrl = url.toString();
+    const routedRpcUrl = urlStr + route;
 
     const rpcResponse = await fetch(routedRpcUrl, {
       method: 'POST',

packages/provider/tests/json-rpc-provider.test.ts
@@ -1,6 +1,6 @@
 import { MockAgent, setGlobalDispatcher } from 'undici'
 import { DEFAULT_URL } from './test-utils'
-import { JsonRpcProvider } from '../src/json-rpc-provider'
+import { JsonRpcProvider, extractBasicAuth } from '../src/json-rpc-provider'
 import { V1RpcRoutes } from '@pokt-foundation/pocketjs-abstract-provider'
 import { responseSamples } from './response-samples'
 import { RawTxRequest } from '@pokt-foundation/pocketjs-types'
@@ -159,5 +159,24 @@ describe('JsonRpcProvider tests', () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     expect(ans).toBe(59133)
+  })
+
+  it('extractBasicAuth test', () => {
+    let {urlStr, basicAuth} =
+    extractBasicAuth('https://localhost:8082');
+    expect(urlStr).toBe('https://localhost:8082');
+    expect(basicAuth).toBeUndefined();
+
+    // Url with a path
+    ({urlStr, basicAuth} =
+      extractBasicAuth('https://mainnet.rpc.grove.city/v1/12345678'));
+    expect(urlStr).toBe('https://mainnet.rpc.grove.city/v1/12345678');
+    expect(basicAuth).toBeUndefined();
+
+    // Url with a path and a credential
+    ({urlStr, basicAuth} =
+      extractBasicAuth('https://scott:tiger@mainnet.rpc.grove.city/v1/12345678'));
+    expect(urlStr).toBe('https://mainnet.rpc.grove.city/v1/12345678');
+    expect(basicAuth).toBe('Basic c2NvdHQ6dGlnZXI=');
   })
 })